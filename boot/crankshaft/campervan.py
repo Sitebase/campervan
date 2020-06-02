f1156eba480c12b9668ae008eea5ae7792ffc3a6
Wim Mostmans <wim@sitebase.be>
2020-06-02 17:38:16 +0200

    Initial version of my button listen script
    This script will already listen to GPIO6 to trigger a reboot.
    This is already a big improvement over the scripts you find online which all seem to use infinite while loops which can be heavy on the CPU.

---
+++ boot/crankshaft/campervan.py
@@ -0,0 +1,38 @@
+!/usr/bin/env python3
+import signal
+import sys
+import subprocess, os
+import RPi.GPIO as GPIO
+
+DISPLAY_SELECTION_GPIO = 6 
+REARCAMERA_GPIO = 5 
+
+print("Press Ctrl & C to Quit")
+
+def signal_handler(sig, frame):
+    print("Quit")
+    GPIO.cleanup()
+    sys.exit(0)
+    
+def button_pressed_callback(channel):
+    print("Button pressed!", channel)
+
+    if channel == REARCAMERA_GPIO:
+        #print(GPIO.input(BUTTON_GPIO))
+        #subprocess.call("logger pressed",shell=True)
+        subprocess.call("crankshaft brightness set 25",shell=True)
+
+    if channel == DISPLAY_SELECTION_GPIO:
+        os.system("sudo reboot")
+
+def handle_buttons():
+    GPIO.setmode(GPIO.BCM)
+
+    # GPIO.setup(REARCAMERA_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # GPIO.add_event_detect(REARCAMERA_GPIO, GPIO.FALLING, callback=button_pressed_callback, bouncetime=300)
+    
+    GPIO.setup(DISPLAY_SELECTION_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(DISPLAY_SELECTION_GPIO, GPIO.BOTH, callback=button_pressed_callback, bouncetime=1000)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.pause()
